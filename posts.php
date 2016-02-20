cbfcab935c6aa6309d2bb3c7316d06037f51895c
Si Thu Aung <sithuaung@ip-172-31-24-111.ec2.internal>
2016-02-20 11:09:55 -0600

    finished combining posts and comments:still need to delete and edit for comment

--- posts.php
+++ posts.php
@@ -8,31 +8,8 @@
 <?php
 require "database.php";
 session_start();
-	$button= $_POST['button'];
-	if(isset($button)){
-		//View
-		if ($button== "View"){
-		 $post_id= $_POST['stories'];
-		 $_SESSION['post_id'] = $post_id;
-		 $stmt= $mysqli->prepare("select post_title,post from posts where post_id=?");
-			if(!$stmt){
-				printf("Query Prep Failed: %s\n", $mysqli->error);
-				exit;
-			}
-			 
-			$stmt->bind_param('i', $post_id);
-			 
-			$stmt->execute();
-			 
-			$stmt->bind_result($post_title, $post);
-			 
-			while ($stmt->fetch()){
-				echo "<h1>".$post_title."</h1>";
-				echo "<p>".$post."</p>";
-			}
-
-			$stmt->close();
-		}
+	if(isset($_POST['button'])){
+		$button= $_POST['button'];
 
 		//Delete
 		if ($button== "Delete"){
@@ -49,11 +26,86 @@ session_start();
 			$stmt->close();
 			header("Location: profile.php");
 		}
+
+		//View
+		if ($button== "View"){
+		 $post_id= $_POST['stories'];
+		 $_SESSION['post_id'] = $post_id;
+		}
+
+		//Comment
+		if ($button== "Comment"){
+			$post_id = $_SESSION['post_id'];
+			$comment= $_POST['comment'];
+			$stmt= $mysqli->prepare("insert into comments (post_id, comment) values (?, ?)");
+
+			if (!$stmt){
+				printf("Query Prep Failed: %s\n", $mysqli->error);
+				exit;
+			}
+
+			$stmt->bind_param('is', $post_id, $comment);
+			$stmt->execute();
+
+			$stmt->close();
+			header("Location: posts.php");
+		}
+	}
+  $post_id = $_SESSION['post_id'];
+  $stmt= $mysqli->prepare("select post_title,post from posts where post_id=?");
+  	if(!$stmt){
+  		printf("Query Prep Failed: %s\n", $mysqli->error);
+  		exit;
+  	}
+  	 
+	$stmt->bind_param('i', $post_id);
+	 
+	$stmt->execute();
+	 
+	$stmt->bind_result($post_title, $post);
+	 
+	while ($stmt->fetch()){
+		echo "<h1>".$post_title."</h1>";
+		echo "<p>".$post."</p>";
 	}
 
+ 	$stmt->close();
+
 ?>
-<form action="comment.php" method="posts">
-	<input type="submit" name="comment" value="comment"/>
+<form action="edit_delete.php" method="posts">
+<?php
+	$post_id= $_SESSION['post_id'];
+	$stmt= $mysqli->prepare("select comment,comment_id from comments where post_id=?");
+	if(!$stmt){
+		printf("Query Prep Failed: %s\n", $mysqli->error);
+		exit;
+	}
+
+	$stmt->bind_param('i', $post_id);
+
+	$stmt->execute();
+
+	$stmt->bind_result($comment, $comment_id);
+
+	while($stmt-> fetch()){
+		echo '<input type="radio" name="stories" value="';
+		echo $comment_id;
+		echo '"/>';
+		printf("\t%s\n<br>",
+			htmlspecialchars($comment)
+		);
+	}
+
+	$stmt->close();
+?>
+	<input type="submit" name="button" value="Edit"/>
+	<input type="submit" name="button" value="Delete"/>
+</form>
+
+<form  method="post">
+	<label> Your Comment</label>
+		<input type="text" name="comment"/>
+		<input type="submit" name="button" value="Comment"/>
 </form>
 
 </body>
